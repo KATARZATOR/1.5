1d57d6e67c25a5deedd6acc9280dddf7929c896e
Ekaterina Belyaeva <belyaeva.e.a@mail.ru>
2024-07-02 22:00:46 +0700

    eee

--- script.js
+++ script.js
@@ -21,12 +21,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const swiper = new Swiper(".mySwiper", {
 
-    slidesPerView: 1.2,
+    breakpoints:{
+        220: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+320: {
+    slidesPerView: 1.3,
+    spaceBetween: 10,
+},
+380: {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+},
+420:{
+    slidesPerView: 1.8,
+    spaceBetween: 10,
+},
+520:{
+    slidesPerView: 2.2,
+    spaceBetween: 10,
+},
+580:{
+    slidesPerView: 2.3,
+    spaceBetween: 10,
+},
+620:{
+    slidesPerView: 2.5,
+    spaceBetween: 10,
+},
+720:{
+    spaceBetween: 10,
+    slidesPerView: 2.8,
+},
+
+    },
+    
   
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+
+    initialSlide:0,
   
   });
 
